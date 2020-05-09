b0f6ae882c517902f44d2449d1524ad1d820d86b
marioasabella <mario.asabella@gmail.com>
2020-05-09 18:27:24 +0200

    Added Dijsktra algorithm

---
+++ src/Algorithms/dijkstra.js
@@ -0,0 +1,54 @@
+const node = {
+  row,
+  col,
+  isVisited,
+  distance,
+};
+
+function dijkstra(grid, startNode, finishNode) {
+  if (!startNode || !finishNode || startNode === finishNode) {
+    return false;
+  }
+
+  nodes[startNode].distance = 0;
+  const unvisitedNodes = nodes.slice();
+  while (!!unvisitedNodes.length) {
+    sortNodesByDistance(unvisitedNodes);
+    const closestNode = unvisitedNodes.unshift();
+    // Handle Walls Later
+    // while(currentNode.status === "wall" && unvisitedNodes.length ) {
+    //     currentNode = getClosestNode(nodes, unvisitedNodes);
+    // }
+    // Handle Impossible Later
+    // if(closestNode.distance === Infinity) return false;
+    // Animate Later
+    // nodesToAnimate.push(closestNode);
+
+    closestNode.isVisited = true;
+    if (closestNode === finishNode) return "success!";
+    updateNeighbors(closestNode, grid);
+  }
+}
+
+function sortNodesByDistance(unvisitedNodes) {
+  unvisitedNodes.sort((nodeA, nodeB) => {
+    nodeA.distance - nodeB.distance;
+  });
+}
+
+function updateNeighbors(node, grid) {
+  const neighbors = getNeighbors(node, grid);
+  for (const neighbor of neighbors) {
+    neighbor.distance = node.distance + 1;
+  }
+}
+
+function getNeighbors(node, grid) {
+  const neighbors = [];
+  const { col, row } = node;
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col + 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+  return neighbors;
+}
