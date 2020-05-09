b15f15bda9ca3b151767e68b6cd0d0d7c89d26de
marioasabella <mario.asabella@gmail.com>
2020-05-09 03:38:54 +0200

    Added pathfinding visualiser and node components

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+
+import './PathfindingVisualizer.css'
+
+export default class PathfindingVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodes: [],
+        };
+    }
+
+    componentDidMount() {
+        const nodes = [];
+        for(let row = 0;  row < 15; row++) {
+            const currentRow = [];
+            for(let col = 0; col < 50; col++) {
+                currentRow.push([]);
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes}); 
+    }
+
+    render() {
+        const {nodes} = this.state;
+        return (
+            <div>
+                Foo
+                <Node></Node>
+            </div>
+        );
+    }
+}
