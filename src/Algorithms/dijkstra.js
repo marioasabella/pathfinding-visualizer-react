bcec323a030d8657c2d1c512fcd97acdd1e1e4f4
marioasabella <mario.asabella@gmail.com>
2020-05-09 22:14:51 +0200

    Refactored dijkstra.js

--- src/Algorithms/dijkstra.js
+++ src/Algorithms/dijkstra.js
@@ -1,54 +1,60 @@
-const node = {
-  row,
-  col,
-  isVisited,
-  distance,
-};
-
-function dijkstra(grid, startNode, finishNode) {
-  if (!startNode || !finishNode || startNode === finishNode) {
-    return false;
-  }
-
-  nodes[startNode].distance = 0;
-  const unvisitedNodes = nodes.slice();
+export function dijkstra(grid, startNode, finishNode) {
+  const visitedNodesInOrder = [];
+  startNode.distance = 0;
+  const unvisitedNodes = getAllNodes(grid);
   while (!!unvisitedNodes.length) {
     sortNodesByDistance(unvisitedNodes);
-    const closestNode = unvisitedNodes.unshift();
-    // Handle Walls Later
-    // while(currentNode.status === "wall" && unvisitedNodes.length ) {
-    //     currentNode = getClosestNode(nodes, unvisitedNodes);
-    // }
-    // Handle Impossible Later
-    // if(closestNode.distance === Infinity) return false;
-    // Animate Later
-    // nodesToAnimate.push(closestNode);
+    const closestNode = unvisitedNodes.shift();
+
+    if (closestNode.isWall) continue;
+
+    if (closestNode.distance === Infinity) return visitedNodesInOrder;
 
     closestNode.isVisited = true;
-    if (closestNode === finishNode) return "success!";
-    updateNeighbors(closestNode, grid);
+    visitedNodesInOrder.push(closestNode);
+    if (closestNode === finishNode) return visitedNodesInOrder;
+    updateUnvisitedNeighbors(closestNode, grid);
   }
 }
 
 function sortNodesByDistance(unvisitedNodes) {
-  unvisitedNodes.sort((nodeA, nodeB) => {
-    nodeA.distance - nodeB.distance;
-  });
+  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
 }
 
-function updateNeighbors(node, grid) {
-  const neighbors = getNeighbors(node, grid);
-  for (const neighbor of neighbors) {
+function updateUnvisitedNeighbors(node, grid) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for (const neighbor of unvisitedNeighbors) {
     neighbor.distance = node.distance + 1;
+    neighbor.previousNode = node;
   }
 }
 
-function getNeighbors(node, grid) {
+function getUnvisitedNeighbors(node, grid) {
   const neighbors = [];
   const { col, row } = node;
   if (row > 0) neighbors.push(grid[row - 1][col]);
   if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
   if (col > 0) neighbors.push(grid[row][col + 1]);
   if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
-  return neighbors;
+  return neighbors.filter((neighbor) => !neighbor.isVisited);
+}
+
+function getAllNodes(grid) {
+  const nodes = [];
+  for (const row of grid) {
+    for (const node of row) {
+      nodes.push(node);
+    }
+  }
+  return nodes;
+}
+
+export function getNodesInShortestPathOrder(finishNode) {
+  const nodesInShortestPathOrder = [];
+  let currentNode = finishNode;
+  while (currentNode !== null) {
+    nodesInShortestPathOrder.unshift(currentNode);
+    currentNode = currentNode.previousNode;
+  }
+  return nodesInShortestPathOrder;
 }
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,41 +1,57 @@
 import React, { Component } from "react";
 import Node from "./Node/Node";
 
+import { dijkstra } from "../Algorithms/dijkstra";
+
 import "./PathfindingVisualizer.css";
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
 
 export default class PathfindingVisualizer extends Component {
   constructor(props) {
     super(props);
     this.state = {
       grid: [],
+      mouseIsPressed: false,
     };
   }
 
   componentDidMount() {
-    const grid = [];
-    for (let row = 0; row < 20; row++) {
-      const currentRow = [];
-      for (let col = 0; col < 50; col++) {
-        const currentNode = {
-          row,
-          col,
-          isStart: row === 10 && col === 5,
-          isFinish: row === 10 && col === 45,
-        };
-        currentRow.push(currentNode);
-      }
-      grid.push(currentRow);
-    }
+    const grid = getInitialGrid();
     this.setState({ grid });
   }
 
+  handleMouseDown(row, col) {
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseIsPressed: true });
+  }
+
+  handleMouseEnter(row, col) {
+    if (!this.state.mouseIsPressed) return;
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseIsPressed: true });
+  }
+
+  handleMouseUp() {
+    this.setState({ grid: newGrid });
+  }
+
+  animateDijsktra(visitedNodesInOrder, nodesInShortestPathOrder) {}
+
   visualiseDijkstra() {
-      const {grid} = this.state;
+    const { grid } = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    // const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    // this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   }
 
   render() {
-    const { grid } = this.state;
-    console.log(grid);
+    const { grid, mouseIsPressed } = this.state;
 
     return (
       <div className="grid">
@@ -59,3 +75,39 @@ export default class PathfindingVisualizer extends Component {
     );
   }
 }
+
+const getInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(row, col));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createNode = (col, row) => {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
+
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
